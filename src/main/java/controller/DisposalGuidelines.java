8f405de50e59bed2de89d3df6d4938a020cb5735
tradebe023 <tradebe023@student.wethinkcode.co.za>
2025-01-31 15:05:18 +0200

    ErrorResponse

--- src/main/java/controller/DisposalGuidelines.java
+++ src/main/java/controller/DisposalGuidelines.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 public class DisposalGuidelines {
     @Autowired
     private DisposalGuidelinesRespository disposalGuidelinesRespository;
