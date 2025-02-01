d32327fbf1e56c48b32a6c072313ef3d33cc47b4
tradebe023 <tradebe023@student.wethinkcode.co.za>
2025-02-01 11:21:14 +0200

    service

--- src/main/java/com/enviro/assessment/grad001/thandekaradebe/wasteSorting/service/RecyclingTipsService.java
+++ src/main/java/com/enviro/assessment/grad001/thandekaradebe/wasteSorting/service/RecyclingTipsService.java
@@ -1,9 +1,27 @@
 package com.enviro.assessment.grad001.thandekaradebe.wasteSorting.service;
 
 
+import com.enviro.assessment.grad001.thandekaradebe.wasteSorting.controller.RecyclingTips;
+import com.enviro.assessment.grad001.thandekaradebe.wasteSorting.repository.RecyclingTipsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RecyclingTipsService {
+    @Autowired
+    private RecyclingTipsRepository recyclingTipsRepository;
+    public List<RecyclingTips> getAllRecyclingTips() {
+        return recyclingTipsRepository.findAll();
+    }
+
+    public RecyclingTips createRecyclingTip(RecyclingTips recyclingTip) {
+        return recyclingTipsRepository.save(recyclingTip);
+    }
+
+    public void deleteRecyclingTip(Long id) {
+        recyclingTipsRepository.deleteById(id);
+    }
 
 }
--- src/main/java/com/enviro/assessment/grad001/thandekaradebe/wasteSorting/service/WasteCategoryService.java
+++ src/main/java/com/enviro/assessment/grad001/thandekaradebe/wasteSorting/service/WasteCategoryService.java
@@ -1,4 +1,34 @@
 package com.enviro.assessment.grad001.thandekaradebe.wasteSorting.service;
 
+import com.enviro.assessment.grad001.thandekaradebe.wasteSorting.model.WasteCategory;
+import com.enviro.assessment.grad001.thandekaradebe.wasteSorting.repository.WasteCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class WasteCategoryService {
+    @Autowired
+    private WasteCategoryRepository wasteCategoryRepository;
+    public List<WasteCategory> getAllWasteCategories() {
+        return wasteCategoryRepository.findAll();
+    }
+
+    public WasteCategory createWasteCategory(WasteCategory wasteCategory) {
+        return wasteCategoryRepository.save(wasteCategory);
+    }
+
+    public void deleteWasteCategory(Long id) {
+         wasteCategoryRepository.deleteById(id);
+    }
+
+    public Optional<WasteCategory> getWasteCategoriesStartingWith(String prefix) {
+        return wasteCategoryRepository.findById(Long.valueOf(prefix));
+    }
+
+
+
+
 }
